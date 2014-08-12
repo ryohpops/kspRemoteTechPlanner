635935a537ffd0219ebc327a8299078e8acfcbfa
ryohpops <ryohpops@users.noreply.github.com>
2014-08-13 08:32:34 +0900

    Update bodyData.

--- kspRemoteTechPlanner/model/antennaData.js
+++ kspRemoteTechPlanner/model/antennaData.js
@@ -15,4 +15,4 @@ var AntennaData;
     }
     AntennaData.getAntenna = getAntenna;
 })(AntennaData || (AntennaData = {}));
-//# sourceMappingURL=antennadata.js.map
+//# sourceMappingURL=antennaData.js.map
