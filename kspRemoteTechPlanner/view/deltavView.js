6e54e224069b68e43db1bede907d0986f5cf89c7
ryohpops <ryohpops@users.noreply.github.com>
2014-08-19 12:47:58 +0900

    Show third canvas and create class for it.

---
+++ kspRemoteTechPlanner/view/deltavView.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../scripts/typings/easeljs/easeljs.d.ts" />
+/// <reference path="../scripts/typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="../scripts/typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="../model/body.ts" />
+/// <reference path="../model/satellites.ts" />
+/// <reference path="../view/graphicshelper.ts" />
+/// <reference path="../view/view.ts" />
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var DeltavView = (function (_super) {
+    __extends(DeltavView, _super);
+    function DeltavView(stage, innerSize, outerSize) {
+        _super.call(this, stage, innerSize, outerSize);
+
+        // shape for drawing in outer coordinates
+        this.shapeOuter = new createjs.Shape();
+        this.shapes.addChild(this.shapeOuter);
+    }
+    DeltavView.bodyRadius = 20;
+    DeltavView.parkingAltitude = 50;
+    DeltavView.designatedAltitude = 150;
+    return DeltavView;
+})(View);
+//# sourceMappingURL=deltavView.js.map
---
+++ kspRemoteTechPlanner/view/deltavView.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../scripts/typings/easeljs/easeljs.d.ts" />
+/// <reference path="../scripts/typings/createjs-lib/createjs-lib.d.ts" />
+/// <reference path="../scripts/typings/tweenjs/tweenjs.d.ts" />
+/// <reference path="../model/body.ts" />
+/// <reference path="../model/satellites.ts" />
+/// <reference path="../view/graphicshelper.ts" />
+/// <reference path="../view/view.ts" />
+
+class DeltavView extends View {
+    private static bodyRadius: number = 20;
+    private static parkingAltitude: number = 50;
+    private static designatedAltitude: number = 150;
+
+    body: Body;
+    satellites: Satellites;
+
+    shapeOuter: createjs.Shape;
+    txtDV1: createjs.Text;
+    txtDV2: createjs.Text;
+
+    constructor(stage: createjs.Stage, innerSize: number, outerSize: number) {
+        super(stage, innerSize, outerSize);
+
+        // shape for drawing in outer coordinates
+        this.shapeOuter = new createjs.Shape();
+        this.shapes.addChild(this.shapeOuter);
+    }
+} 
