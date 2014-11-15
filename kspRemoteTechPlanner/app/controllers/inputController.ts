7ac23c98b6db3e2f4f19ddced283c07cb9b16cea
ryohpops <ryohpops@users.noreply.github.com>
2014-11-15 18:00:08 +0900

    Create InputController.

---
+++ kspRemoteTechPlanner/app/controllers/inputController.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../appreferences.ts" />
+module App {
+    export class InputController {
+        'use strict';
+
+        static $inject = ["$scope", "satChainServ"];
+        constructor(
+            private $scope: ng.IScope,
+            private satChainServ: SatChainService
+            ) {
+
+        }
+    }
+}
