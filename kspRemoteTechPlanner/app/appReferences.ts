cf22d44342538a2f761d2b8338ac6551708bd91a
ryohpops <ryohpops@users.noreply.github.com>
2014-12-15 17:12:27 +0900

    Start handling Entire view.

--- kspRemoteTechPlanner/app/appReferences.ts
+++ kspRemoteTechPlanner/app/appReferences.ts
@@ -7,6 +7,7 @@
 /// <reference path="services/antennastorageservice.ts" />
 /// <reference path="services/bodystorageservice.ts" />
 /// <reference path="services/satchainservice.ts" />
+/// <reference path="services/entireview.ts" />
 
 /// <reference path="controllers/inputcontroller.ts" />
 /// <reference path="controllers/appcontroller.ts" />
---
+++ kspRemoteTechPlanner/app/services/entireView.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../appreferences.ts" />
+module App {
+    export class EntireView {
+        'use strict';
+
+        static $inject = ["$element", "satChainServ"];
+        constructor(
+            private $element: JQuery,
+            private satChainServ: SatChainService
+            ) {
+
+        }
+    }
+}
