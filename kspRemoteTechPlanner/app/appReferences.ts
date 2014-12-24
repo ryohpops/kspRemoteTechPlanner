f57b15beddf7481ccfe799b509c4fbc92eff6886
ryohpops <ryohpops@users.noreply.github.com>
2014-12-24 16:12:40 +0900

    Reference order is now filename-based. Note that any class which are extended by others should be ordered first in same-folder group.

--- kspRemoteTechPlanner/app/appReferences.ts
+++ kspRemoteTechPlanner/app/appReferences.ts
@@ -4,15 +4,16 @@
 /// <reference path="models/body.ts" />
 /// <reference path="models/satchain.ts" />
 
-/// <reference path="services/bodystorageservice.ts" />
-/// <reference path="services/antennastorageservice.ts" />
-/// <reference path="services/satchainservice.ts" />
-/// <reference path="services/graphicshelperservice.ts" />
 /// <reference path="services/viewservice.ts" />
-/// <reference path="services/entireviewservice.ts" />
-/// <reference path="services/nightviewservice.ts" />
+
+/// <reference path="services/antennastorageservice.ts" />
+/// <reference path="services/bodystorageservice.ts" />
 /// <reference path="services/deltavviewservice.ts" />
+/// <reference path="services/entireviewservice.ts" />
+/// <reference path="services/graphicshelperservice.ts" />
+/// <reference path="services/nightviewservice.ts" />
+/// <reference path="services/satchainservice.ts" />
 
 /// <reference path="controllers/appcontroller.ts" />
-/// <reference path="controllers/inputcontroller.ts" />
 /// <reference path="controllers/bodyeditcontroller.ts" />
+/// <reference path="controllers/inputcontroller.ts" />
