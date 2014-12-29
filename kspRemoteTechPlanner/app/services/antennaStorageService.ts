118915ce3fb3738ca311cf4007de749a5e39bcee
ryohpops <ryohpops@users.noreply.github.com>
2014-12-29 22:04:47 +0900

    Add all dishes.

--- kspRemoteTechPlanner/app/services/antennaStorageService.ts
+++ kspRemoteTechPlanner/app/services/antennaStorageService.ts
@@ -30,7 +30,11 @@ module App {
                 "CommTech EXP-VR-2T": new Antenna("CommTech EXP-VR-2T", AntennaType.omni, 3000, 0.18),
                 "Communotron 32": new Antenna("Communotron 32", AntennaType.omni, 5000, 0.6),
                 "Comms DTS-M1": new Antenna("Comms DTS-M1", AntennaType.dish, 50000, 0.82),
-                "Reflectron KR-7": new Antenna("Reflectron KR-7", AntennaType.dish, 90000, 0.82)
+                "Reflectron KR-7": new Antenna("Reflectron KR-7", AntennaType.dish, 90000, 0.82),
+                "Communotron 88-88": new Antenna("Communotron 88-88", AntennaType.dish, 40000000, 0.93),
+                "Reflectron KR-14": new Antenna("Reflectron KR-14", AntennaType.dish, 60000000, 0.93),
+                "CommTech-1": new Antenna("CommTech-1", AntennaType.dish, 350000000, 2.6),
+                "Reflectron GX-128": new Antenna("Reflectron GX-128", AntennaType.dish, 400000000, 2.8)
             };
 
             this._userAntennas = this.loadOrCreate();
