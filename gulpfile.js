057fe14325b4c584e020de2bc412950d35f4b005
ryohpops <ryohpops@users.noreply.github.com>
2015-01-06 18:08:17 +0900

    Improve deploy script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 'use strict';
 
-var rimraf = require("rimraf");
 var gulp = require("gulp");
 var minjs = require("gulp-uglify");
 var mincss = require("gulp-minify-css");
+var rimraf = require("rimraf");
 
-var destDir = "deploy/";
+var deployDir = "deploy/";
 
 gulp.task("clean", function (cb) {
-    rimraf(destDir, cb);
+    rimraf(deployDir, cb);
 });
 
-gulp.task("deploy", ["clean"], function () {
-    gulp.src("kspRemoteTechPlanner/**/*.html")
-    .pipe(gulp.dest(destDir));
+gulp.task("copy-html", ["clean"], function () {
+    return gulp.src("kspRemoteTechPlanner/**/*.html")
+    .pipe(gulp.dest(deployDir));
+});
 
-    gulp.src("kspRemoteTechPlanner/**/appOut.js")
+gulp.task("minify-js", ["clean"], function () {
+    return gulp.src("kspRemoteTechPlanner/**/appOut.js")
     .pipe(minjs())
-    .pipe(gulp.dest(destDir));
-
-    gulp.src("kspRemoteTechPlanner/**/app.css")
-    .pipe(mincss())
-    .pipe(gulp.dest(destDir));
+    .pipe(gulp.dest(deployDir));
 });
+
+gulp.task("minify-css", ["clean"], function () {
+    return gulp.src("kspRemoteTechPlanner/**/app.css")
+    .pipe(mincss())
+    .pipe(gulp.dest(deployDir));
+});
+
+gulp.task("deploy", ["copy-html", "minify-js", "minify-css"], function () { });
