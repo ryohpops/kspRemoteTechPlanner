3310d365659cb47cc87d3fa3080357ba22e51eff
ryohpops <ryohpops@users.noreply.github.com>
2015-01-07 21:05:10 +0900

    Add tests related to select elements.

--- kspRemoteTechPlanner/test/spec.js
+++ kspRemoteTechPlanner/test/spec.js
@@ -1,7 +1,43 @@
-describe("index.html", function () {
-    it("should show correct title", function () {
-        browser.get("http://localhost:63269/index.html");
+describe("Data Input", function () {
+    beforeEach(function () {
+        browser.get("http://localhost:63269/");
+    });
 
-        expect(browser.getTitle()).toEqual("Visual RemoteTech Planner for KSP");
+    describe("selecting body", function () {
+        var bodySelector = element(by.xpath("//select[@ng-model='input.satChain.body.name']"));
+        var bodyDetailToggle = element(by.xpath("//button[@data-target='#detail_body']"));
+
+        it("should load body's detail", function () {
+            bodySelector.element(by.xpath("//option[text()='Moho']")).click();
+            bodyDetailToggle.click();
+
+            expect(element(by.binding("input.satChain.body.color")).getText()).toEqual("rgb(185,122,87)");
+            expect(element(by.binding("input.satChain.body.radius")).getText()).toEqual("250 km");
+            expect(element(by.binding("input.satChain.body.stdGravity")).getText()).toEqual("168.609 km3s-2");
+            expect(element(by.binding("input.satChain.body.soi")).getText()).toEqual("9,646.663 km");
+        });
+    });
+
+    describe("selecting antenna", function () {
+        var antennaSelector = element(by.xpath("//select[@ng-model='input.satChain.antenna.name']"));
+        var antennaDetailToggle = element(by.xpath("//button[@data-target='#detail_antenna']"));
+
+        it("should load omni-antenna's detail", function () {
+            antennaSelector.element(by.xpath("//option[text()='Communotron 32']")).click();
+            antennaDetailToggle.click();
+
+            expect(element(by.binding("input.satChain.antenna.typeString()")).getText()).toEqual("Omnidirectional");
+            expect(element(by.binding("input.satChain.antenna.range")).getText()).toEqual("5,000 km");
+            expect(element(by.binding("input.satChain.antenna.elcNeeded")).getText()).toEqual("0.6 /sec.");
+        });
+
+        it("should load dish-antenna's detail", function () {
+            antennaSelector.element(by.xpath("//option[text()='Reflectron KR-7']")).click();
+            antennaDetailToggle.click();
+
+            expect(element(by.binding("input.satChain.antenna.typeString()")).getText()).toEqual("Dish");
+            expect(element(by.binding("input.satChain.antenna.range")).getText()).toEqual("90,000 km");
+            expect(element(by.binding("input.satChain.antenna.elcNeeded")).getText()).toEqual("0.82 /sec.");
+        })
     });
 });
