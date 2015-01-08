35ef9436d892721f30424525c731b73da61a1e64
ryohpops <ryohpops@users.noreply.github.com>
2015-01-08 19:22:04 +0900

    Add spec for remember-last-state function of Input Data.

--- kspRemoteTechPlanner/test/spec.js
+++ kspRemoteTechPlanner/test/spec.js
@@ -1,12 +1,15 @@
 describe("Data Input", function () {
+    var bodySelector = element(by.xpath("//select[@ng-model='input.satChain.body.name']"));
+    var bodyDetailToggle = element(by.xpath("//button[@data-target='#detail_body']"));
+
+    var antennaSelector = element(by.xpath("//select[@ng-model='input.satChain.antenna.name']"));
+    var antennaDetailToggle = element(by.xpath("//button[@data-target='#detail_antenna']"));
+
     beforeEach(function () {
         browser.get("http://localhost:63269/");
     });
 
     describe("selecting body", function () {
-        var bodySelector = element(by.xpath("//select[@ng-model='input.satChain.body.name']"));
-        var bodyDetailToggle = element(by.xpath("//button[@data-target='#detail_body']"));
-
         it("should load body's detail", function () {
             bodySelector.element(by.xpath("//option[text()='Moho']")).click();
             bodyDetailToggle.click();
@@ -19,9 +22,6 @@ describe("Data Input", function () {
     });
 
     describe("selecting antenna", function () {
-        var antennaSelector = element(by.xpath("//select[@ng-model='input.satChain.antenna.name']"));
-        var antennaDetailToggle = element(by.xpath("//button[@data-target='#detail_antenna']"));
-
         it("should load omni-antenna's detail", function () {
             antennaSelector.element(by.xpath("//option[text()='Communotron 32']")).click();
             antennaDetailToggle.click();
@@ -39,5 +39,29 @@ describe("Data Input", function () {
             expect(element(by.binding("input.satChain.antenna.range")).getText()).toEqual("90,000 km");
             expect(element(by.binding("input.satChain.antenna.elcNeeded")).getText()).toEqual("0.82 /sec.");
         })
+    });
+
+    it("should remember last input", function () {
+        var count = element(by.xpath("//input[@ng-model='input.satChain.count']"));
+        var altitude = element(by.xpath("//input[@ng-model='input.satChain.altitude']"));
+        var elcNeeded = element(by.xpath("//input[@ng-model='input.satChain.elcNeeded']"));
+        var parkingAlt = element(by.xpath("//input[@ng-model='input.satChain.parkingAlt']"));
+
+        bodySelector.element(by.xpath("//option[text()='Eve']")).click();
+        count.sendKeys(protractor.Key.chord(protractor.Key.CONTROL, "a"), "6");
+        altitude.sendKeys(protractor.Key.chord(protractor.Key.CONTROL, "a"), "500");
+        elcNeeded.sendKeys(protractor.Key.chord(protractor.Key.CONTROL, "a"), "0.88");
+        antennaSelector.element(by.xpath("//option[text()='CommTech EXP-VR-2T']")).click();
+        parkingAlt.sendKeys(protractor.Key.chord(protractor.Key.CONTROL, "a"), "100");
+        browser.waitForAngular();
+
+        browser.refresh();
+
+        expect(bodySelector.getAttribute("value")).toEqual("Eve");
+        expect(count.getAttribute("value")).toEqual("6");
+        expect(altitude.getAttribute("value")).toEqual("500");
+        expect(elcNeeded.getAttribute("value")).toEqual("0.88");
+        expect(antennaSelector.getAttribute("value")).toEqual("CommTech EXP-VR-2T");
+        expect(parkingAlt.getAttribute("value")).toEqual("100");
     });
 });
