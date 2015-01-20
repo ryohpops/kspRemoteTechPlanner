459bf092e9ac0318a25054513a4467d54370b7f8
ryohpops <ryohpops@users.noreply.github.com>
2015-01-20 19:46:03 +0900

    Control task flow by run-sequence.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var minjs = require("gulp-uglify");
 var mincss = require("gulp-minify-css");
 var connect = require("gulp-connect");
 var shell = require("gulp-shell");
+var sequence = require("run-sequence");
 var rimraf = require("rimraf");
 
 var targetConf = "kspRemoteTechPlannerTest/conf.js";
@@ -21,35 +22,39 @@ gulp.task("server-start", function () {
     });
 });
 
-gulp.task("protractor", ["wdm-update", "server-start"],
-    shell.task("protractor " + targetConf, { ignoreErrors: true })
-);
-
-gulp.task("server-stop", ["protractor"], function () {
+gulp.task("server-stop", function () {
     connect.serverClose();
 });
 
-gulp.task("test", ["server-stop"], function () { });
+gulp.task("protractor",
+    shell.task("protractor " + targetConf, { ignoreErrors: true })
+);
+
+gulp.task("test", function (cb) {
+    sequence(["wdm-update", "server-start"], "protractor", "server-stop", cb);
+});
 
 gulp.task("clean", function (cb) {
     rimraf(deployDir, cb);
 });
 
-gulp.task("copy-html", ["clean"], function () {
+gulp.task("copy-html", function () {
     return gulp.src("kspRemoteTechPlanner/**/*.html")
     .pipe(gulp.dest(deployDir));
 });
 
-gulp.task("minify-js", ["clean"], function () {
+gulp.task("minify-js", function () {
     return gulp.src("kspRemoteTechPlanner/**/appOut.js")
     .pipe(minjs())
     .pipe(gulp.dest(deployDir));
 });
 
-gulp.task("minify-css", ["clean"], function () {
+gulp.task("minify-css", function () {
     return gulp.src("kspRemoteTechPlanner/**/app.css")
     .pipe(mincss())
     .pipe(gulp.dest(deployDir));
 });
 
-gulp.task("deploy", ["copy-html", "minify-js", "minify-css"], function () { });
+gulp.task("deploy", function (cb) {
+    sequence("clean", ["copy-html", "minify-js", "minify-css"], cb);
+});
