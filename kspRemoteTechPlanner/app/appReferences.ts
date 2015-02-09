b1d4543747755011b0a1c3e7497f39705cbae53d
ryohpops <ryohpops@users.noreply.github.com>
2015-02-09 11:15:43 +0900

    Move Calculator module.

--- kspRemoteTechPlanner/app/appReferences.ts
+++ kspRemoteTechPlanner/app/appReferences.ts
@@ -1,6 +1,4 @@
-﻿/// <reference path="calculator/calculatorreferences.ts" />
-
-/// <reference path="models/antenna.ts" />
+﻿/// <reference path="models/antenna.ts" />
 /// <reference path="models/body.ts" />
 /// <reference path="models/satchain.ts" />
 
