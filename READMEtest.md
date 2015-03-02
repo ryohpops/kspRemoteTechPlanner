09a02af3f99488e8c11da85323df88c6a63841ad
ryohpops <ryohpops@users.noreply.github.com>
2015-03-03 08:59:45 +0900

    Create copy task.

---
+++ READMEtest.md
@@ -0,0 +1,6 @@
+kspRemoteTechPlanner
+====================
+
+Web-based satellite network planner for the MOD, [RemoteTech](http://forum.kerbalspaceprogram.com/threads/83305).
+
+Latest release is available [here](http://ryohpops.github.io/kspRemoteTechPlanner/) and forum is [here](http://forum.kerbalspaceprogram.com/threads/90113).
---
+++ kspRemoteTechPlannerTool/tasks/make-readme.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="../_references.ts" />
+
+'use strict';
+
+import gulp = require("gulp");
+var sequence: Function = require("run-sequence");
+import fs = require("fs");
+import path = require("path");
+import rename = require("gulp-rename");
+
+var header: string = "doc/header.md";
+var changeLog: string = "doc/changelog.md";
+var dest: string = "../READMEtest.md";
+
+gulp.task("make-readme:copy-header",() => {
+    return gulp.src(path.normalize(header))
+        .pipe(rename(path.basename(dest)))
+        .pipe(gulp.dest(path.dirname(dest)));
+});
