de15a037f621d6cf2cb68af93dfe39fb16b2f966
ryohpops <ryohpops@users.noreply.github.com>
2015-03-03 10:38:29 +0900

    Complete implementation.

--- READMEtest.md
+++
@@ -1,6 +0,0 @@
-kspRemoteTechPlanner
-====================
-
-Web-based satellite network planner for the MOD, [RemoteTech](http://forum.kerbalspaceprogram.com/threads/83305).
-
-Latest release is available [here](http://ryohpops.github.io/kspRemoteTechPlanner/) and forum is [here](http://forum.kerbalspaceprogram.com/threads/90113).
--- kspRemoteTechPlannerTool/tasks/make-readme.ts
+++ kspRemoteTechPlannerTool/tasks/make-readme.ts
@@ -5,15 +5,25 @@
 import gulp = require("gulp");
 var sequence: Function = require("run-sequence");
 import fs = require("fs");
+import os = require("os");
 import path = require("path");
 import rename = require("gulp-rename");
 
-var header: string = "doc/header.md";
-var changeLog: string = "doc/changelog.md";
-var dest: string = "../READMEtest.md";
+var header: string = "../docs/header.md";
+var changeLog: string = "../docs/changelog.md";
+var dest: string = "../README.md";
 
 gulp.task("make-readme:copy-header",() => {
     return gulp.src(path.normalize(header))
         .pipe(rename(path.basename(dest)))
         .pipe(gulp.dest(path.dirname(dest)));
+});
+
+gulp.task("make-readme:append-last-change",(cb: gulp.ITaskCallback) => {
+    var log: string[] = fs.readFileSync(path.normalize(changeLog), "utf8").split(os.EOL + "-");
+    fs.appendFile(path.normalize(dest), os.EOL + log[0], cb);
+});
+
+gulp.task("make-readme",(cb: gulp.ITaskCallback) => {
+    sequence("make-readme:copy-header", "make-readme:append-last-change", cb);
 });
