9490ddfc084c554f8787cd29ebc00cf780940ad2
ryohpops <ryohpops@users.noreply.github.com>
2015-04-10 13:03:34 +0900

    Implement Night View.

--- kspRemoteTechPlanner/app/appReferences.ts
+++ kspRemoteTechPlanner/app/appReferences.ts
@@ -11,13 +11,13 @@
 /// <reference path="services/bodydictionaryservice.ts" />
 /// <reference path="services/deltavviewservice.ts" />
 /// <reference path="services/graphicshelperservice.ts" />
-/// <reference path="services/nightviewservice.ts" />
 /// <reference path="services/satchainservice.ts" />
 
 /// <reference path="controllers/bodyeditcontroller.ts" />
 /// <reference path="controllers/antennaeditcontroller.ts" />
 /// <reference path="controllers/entireviewcontroller.ts" />
 /// <reference path="controllers/inputcontroller.ts" />
+/// <reference path="controllers/nightviewcontroller.ts" />
 
 /// <reference path="filters/antennatype.ts" />
 /// <reference path="filters/scale.ts" />
---
+++ kspRemoteTechPlanner/app/controllers/nightViewController.ts
@@ -0,0 +1,45 @@
+﻿/// <reference path="../appreferences.ts" />
+
+module App {
+    export class NightViewController {
+        'use strict';
+
+        sc: SatChain;
+        body: Body;
+        get period(): number {
+            return this.orbitalServ.period(this.body.radius, this.sc.altitude, this.body.stdGravity);
+        }
+        get nightTime(): number {
+            return this.orbitalServ.nightTime(this.body.radius, this.sc.altitude, this.body.stdGravity);
+        }
+        get reqGen(): number {
+            var ae: number[] = [], aq: number[] = [];
+            for (var index in this.sc.antennas) {
+                ae.push(this.sc.antennas[index].antenna.elcNeeded);
+                aq.push(this.sc.antennas[index].quantity);
+            }
+
+            return this.satelliteServ.requiredGenerator(this.sc.elcNeeded, ae, aq, this.body.radius, this.body.stdGravity, this.sc.altitude);
+        }
+        get reqBatt(): number {
+            var ae: number[] = [], aq: number[] = [];
+            for (var index in this.sc.antennas) {
+                ae.push(this.sc.antennas[index].antenna.elcNeeded);
+                aq.push(this.sc.antennas[index].quantity);
+            }
+
+            return this.satelliteServ.requiredBattery(this.sc.elcNeeded, ae, aq, this.body.radius, this.body.stdGravity, this.sc.altitude);
+        }
+
+        static $inject = ["satChainServ", "calc.orbitalServ", "calc.satelliteServ"];
+        constructor(
+            private satChainServ: SatChainService,
+            private orbitalServ: Calculator.OrbitalService,
+            private satelliteServ: Calculator.SatelliteService
+            ) {
+
+            this.sc = this.satChainServ.satChain;
+            this.body = this.sc.body;
+        }
+    }
+}
