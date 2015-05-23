ef6bd4d05ca7f4bf7a86e9d37efbdf80b79d5c4a
ryohpops <ryohpops@users.noreply.github.com>
2015-05-23 12:59:13 +0900

    Remove unused variables in Input Controller.

--- kspRemoteTechPlanner/app/controllers/inputController.ts
+++ kspRemoteTechPlanner/app/controllers/inputController.ts
@@ -5,8 +5,6 @@ module App {
 
     export class InputController {
         sc: SatChain;
-        bodyDetailVisible: boolean;
-        antennaDetailVisible: boolean[];
 
         static $inject = ["eventServ", "satChainServ", "bodyDictServ", "antennaDictServ"];
         constructor(
@@ -17,10 +15,6 @@ module App {
             ) {
 
             this.sc = this.satChainServ.satChain;
-            this.bodyDetailVisible = false;
-            this.antennaDetailVisible = new Array<boolean>();
-            for (var i: number = 0; i < this.sc.antennas.length; i++)
-                this.antennaDetailVisible.push(false);
         }
 
         isSelectedAntenna(index: number): boolean {
@@ -49,13 +43,11 @@ module App {
 
         onAddAntenna() {
             this.sc.antennas.push({ antenna: this.antennas.get("Reflectron DP-10"), quantity: 1 });
-            this.antennaDetailVisible.push(false);
             this.onAntennaChange();
         }
 
         onRemoveSelectedAntenna() {
             this.sc.antennas.splice(this.sc.antennaIndex, 1);
-            this.antennaDetailVisible.splice(this.sc.antennaIndex, 1);
             if (this.sc.antennaIndex === this.sc.antennas.length)
                 this.sc.antennaIndex--;
             this.onAntennaChange();
